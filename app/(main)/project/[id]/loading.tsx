8c2ee20059d772dbac4ed978b36c0dbdcb803396
Oz996 <ozgun996@hotmail.com>
2024-11-22 14:03:23 +0100

    trying loading ui for project page

---
+++ app/(main)/project/[id]/loading.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Container from "./components/container";
+
+export default function Loading() {
+  return (
+    <Container>
+      <div className="pt-20">Loading...</div>
+    </Container>
+  );
+}
